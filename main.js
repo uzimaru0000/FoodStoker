1176ac54183880b713759bad74fb30c95acc880c
uzimaru <uzimaru0601@gmail.com>
2017-05-21 15:23:57 +0900

    リップルエフェクトを実装

--- main.js
+++ main.js
@@ -5,14 +5,27 @@ const app = new Vue({
     data: {
         appName: "FoodStoker",
         dates: [
-            {
-                name: "にんじん",
-                deadLine: "2017-06-01"
-            },
-            {
-                name: "じゃがいも",
-                deadLine: "2017-06-06"
-            },
-        ]
+            
+        ],
+        buttonFlag: false
+    },
+    methods: {
+        addList: function(e) {
+            let rect = e.target.getBoundingClientRect();
+            let span = document.createElement('div');
+            span.setAttribute('class', 'ripple');
+            span.setAttribute('style',
+                styleAttribute('top', (e.y - rect.top - (rect.height / 2)) + 'px') +
+                styleAttribute('left', (e.x - rect.left - (rect.width / 2)) + 'px')
+            );
+            e.target.appendChild(span);
+            span.addEventListener('webkitAnimationEnd', () => {
+                e.target.removeChild(span);
+            });
+        }
     }
 });
+
+const styleAttribute = (name, value) => {
+    return name + ':' + value + ';';
+};
